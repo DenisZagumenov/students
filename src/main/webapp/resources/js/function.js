ff4bd4d827448819d72ded304f69fdad6da7e8cb
Denis Z <thebestden@ya.ru>
2023-03-14 22:46:41 +0400

    Discipline deleting and modifying is implemebted

--- src/main/webapp/resources/js/function.js
+++ src/main/webapp/resources/js/function.js
@@ -1,4 +1,4 @@
-function deleteStudents() {
+﻿function deleteStudents() {
 
     const checkedCheckBoxes = document.querySelectorAll("input[name=studentId]:checked");
 
@@ -35,4 +35,43 @@ function modifyStudent() {
 
     document.getElementById("idsForModify").value = id;
     document.getElementById("modifyForm").submit();
+}
+
+function modifyDiscipline() {
+
+    const checkedCheckBoxes = document.querySelectorAll("input[name=disciplineId]:checked");
+
+    if (checkedCheckBoxes.length == 0) {
+        alert("Пожалуйста, выберите дисциплину");
+        return;
+    }
+
+    if (checkedCheckBoxes.length > 1) {
+        alert("Пожалуйста, выберите только одну дисциплину");
+        return;
+    }
+
+    let id = checkedCheckBoxes[0].value;
+
+    document.getElementById("idsForModifyDiscipline").value = id;
+    document.getElementById("modifyFormDiscipline").submit();
+}
+
+function deleteDiscipline() {
+
+    const checkedCheckBoxes = document.querySelectorAll("input[name=disciplineId]:checked");
+
+    if (checkedCheckBoxes.length == 0) {
+        alert("Пожалуйста, выберите хотя бы одну дисциплину");
+        return;
+    }
+
+    let ids = "";
+
+    for (let i = 0; i < checkedCheckBoxes.length; i++) {
+        ids = ids + checkedCheckBoxes[i].value + " ";
+    }
+
+    document.getElementById("idsForDeleteDiscipline").value = ids;
+    document.getElementById("deleteFormDiscipline").submit();
 }
